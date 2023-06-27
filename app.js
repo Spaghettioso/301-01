7a1ae9a83c3865d611e9ca4c73e466d0f9b601c2
Jonathan OReilly <jonathanoreilly@live.co.uk>
2023-06-27 08:04:09 +0100

    finished js script

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const image3 = document.querySelector("section img:nth-child(3)");
 
 let clicks = 0;
 const maxClicksAllowed = 5;
+//creating an empty array that we push our list of products into later on
 let allProducts = [];
 
 function randomNumberGenerator() {
@@ -22,13 +23,92 @@ function Product(name, src, timesShown) {
 }
 
 function renderProducts() {
+  //assigning values to the 3 products that will be displayed on page
   let product1 = randomNumberGenerator();
   let product2 = randomNumberGenerator();
   let product3 = randomNumberGenerator();
-
-  while (product1 === product2 || product1 === product3) {
+  //while loop to prevent getting the same number twice, if any are the same, it runs random number function again
+  while (
+    product1 === product2 ||
+    product1 === product3 ||
+    product2 === product3
+  ) {
     product1 = randomNumberGenerator();
     product2 = randomNumberGenerator();
     product3 = randomNumberGenerator();
   }
+  //giving attributes to image1,2,3 using the details from the images
+  image1.src = allProducts[product1].src;
+  image2.src = allProducts[product2].src;
+  image2.src = allProducts[product3].src;
+  image1.alt = allProducts[product1].name;
+  image2.alt = allProducts[product2].name;
+  image3.alt = allProducts[product3].name;
+  //when function is run we are adding 1 to the views
+  allProducts[product1].views++;
+  allProducts[product2].views++;
+  allProducts[product3].views++;
 }
+
+function handleProductClick(event) {
+  if (event.target === productsSection) {
+    alert("Please click on an image");
+  } else {
+    clicks++;
+    let clickedProduct = event.target.alt;
+    for (let i = o; i < allProducts.length; i++) {
+      if (clickedProduct === allProducts[i].name) {
+        allProducts[i].clicks++;
+        break;
+      }
+    }
+    if (clicks === maxClicksAllowed) {
+      productsSection.removeEventListener("click", handleProductClick);
+      productsSection.className = "no-voting";
+      resultsButton.addEventListener("click", renderResults);
+      resultsButton.className = "clicks-allowed";
+    } else {
+      renderProducts();
+    }
+  }
+}
+
+function renderResults() {
+  let ul = document.createElement("ul");
+  for (let i = 0; i < allProducts.length; i++) {
+    let li = document.createElement("li");
+    li.textContent =
+      "${allProducts[i].name} had ${allProducts[i].views} views and was clicked ${allProducts[i].clicks} times.";
+    ul.appendChild(li);
+  }
+}
+
+const productNames = [
+  "bag",
+  "banana",
+  "bathroom",
+  "boots",
+  "breakfast",
+  "bubblegum",
+  "chair",
+  "cthulu",
+  "dog-duck",
+  "dragon",
+  "pen",
+  "pet-sweep",
+  "scissors",
+  "shark",
+  "sweep",
+  "tauntaun",
+  "unicorn",
+  "water-can",
+  "wine-glass",
+];
+
+renderProducts();
+
+for (let i = 0; i < productNames.length; i++) {
+  new Product(productNames[i], `imgs/${productNames[i]}.jpg`);
+}
+
+productsSection.addEventListener("click", handleProductClick);
